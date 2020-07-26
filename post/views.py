62ffee9c1e53384e77125c10b1a2f3101fb82290
awesomeo184 <awesomeo184@gmail.com>
2020-07-27 02:58:18 +0900

    post list, detail, create

--- post/views.py
+++ post/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your views here.
+from post.forms import PostForm
+from post.models import Post
+
+
+def list(request):
+    query_set = Post.objects.all()
+    context = {
+        'posts': query_set
+    }
+    return render(request, 'post/list.html', context)
+
+
+def detail(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'post/detail.html', context)
+
+
+def create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.created_at = timezone.now()
+            post.save()
+            pk = post.id
+            url = reverse('post:detail', kwargs={'pk': pk})
+            return redirect(to=url)
+    else:
+        form = PostForm()
+    context = {'form': form}
+    return render(request, 'post/create.html', context)
